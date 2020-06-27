a35e51df191c748d42df888719e2784e45c80534
andrasetiabudi <andraa.jp@gmail.com>
2020-06-27 08:40:36 +0700

    upload project 1

---
+++ darkMode.js
@@ -0,0 +1,12 @@
+
+
+
+/*function toggleDarkLight() {
+  var body = document.getElementById("body");
+  var currentClass = body.className;
+  body.className = currentClass == "dark-mode" ? "light-mode" : "dark-mode";
+}*/
+function darkMode() {
+      var element = document.body;
+      element.classList.toggle("dark-mode");
+    }
